b8f125cf6b940b754cafe14779a24d38c7849048
Brandon Canaday <brandon.canaday@yahoo.com>
2017-09-27 12:52:35 -0400

    added process.env.PORT || 3000 to listen() function

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', function(req, res) {
 
 io.on('connection', onConnection);
 
-http.listen(3000, function() {
+http.listen(process.env.PORT || 3000, function() {
   console.log('listening on *:3000');
 });
 
